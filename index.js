f2b611fc71981052d5e24c8de20fbf6484f700b9
Long Hoang <long88hoang@gmail.com>
2022-10-25 12:53:59 -0500

    Added Wild Wild Life function

--- index.js
+++ index.js
@@ -23,32 +23,47 @@
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-const getMyNumbers = () =>{
-    let num;
-        for(let i=1; i<101; i++){
-            num=i;
+// const getMyNumbers = () =>{
+//     let num;
+//         for(let i=1; i<101; i++){
+//             num=i;
         
-        if(num%3===0 && num%5===0){
-            console.log(`FizzBuzz`);
-        }
-        else if(num%3===0){
-            console.log(`Fizz`);
-        }
-        else if(num%5===0){
-            console.log(`Buzz`);
-        }
-        else {
-            console.log(num);
-        }
-    }
-}
-getMyNumbers();
-
+//         if(num%3===0 && num%5===0){
+//             console.log(`FizzBuzz`);
+//         }
+//         else if(num%3===0){
+//             console.log(`Fizz`);
+//         }
+//         else if(num%5===0){
+//             console.log(`Buzz`);
+//         }
+//         else {
+//             console.log(num);
+//         }
+//     }
+// }
+// getMyNumbers();
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee[2] += 1;
+// console.log(plantee)
+
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy[3] = 'Gotham City'
+// console.log(wolfy)
+// Give D'Art a second hometown by adding "Hawkins"
+// dart[3] += "Hawkins"
+// console.log(dart)
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
